1c2a89e3ba78ce7f11874a14b4784f4dbe952fbf
YUST777 <YUST777@users.noreply.github.com>
2025-10-31 11:56:47 +0000

    fix: Bottom nav always on top of side drawers + desktop full width
    - Add z-[60] to DynamicNavigation to stay above drawers (z-50)
    - Change desktop container from max-w-4xl to max-w-full
    - Change desktop mx-auto to mx-0 to fill width
    - Bottom nav now accessible even when drawers open
    - Desktop uses full screen width

--- webapp-nextjs/src/components/MainApp.tsx
+++ webapp-nextjs/src/components/MainApp.tsx
@@ -271,7 +271,7 @@ export const MainApp: React.FC = () => {
       
       {/* Main Content */}
       <main className="pb-28 lg:pb-6">
-        <div className="max-w-md mx-auto px-4 lg:max-w-4xl lg:px-6">
+        <div className="max-w-md mx-auto px-4 lg:max-w-full lg:mx-0 lg:px-6">
           <div className="transition-all duration-300 ease-in-out">
             {renderTabContent()}
           </div>
